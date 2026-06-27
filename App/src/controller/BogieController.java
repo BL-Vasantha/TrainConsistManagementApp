79c708287deb64f83e742c03718a1e6fec81f77b
BL-Vasantha <vasantha.ragimekalapalli@bridgelabz.com>
2026-06-27 14:44:49 +0530

    [VASANTHA] added UC15:Bogie Controller

--- App/src/controller/BogieController.java
+++ App/src/controller/BogieController.java
@@ -6,7 +6,6 @@ import service.BogieServiceImpl;
 
 import java.util.List;
 
-
 public class BogieController {
 
     private final BogieService service;
@@ -19,9 +18,11 @@ public class BogieController {
         service.addBogie(new Bogie(name, displayCount, actualSeats));
     }
 
+
     public List<Bogie> getAllBogies() {
         return service.getAllBogies();
     }
+
 
     public int getTotalCapacity() {
         return service.getTotalSeatingCapacity();
