b7acab1cc258e71958cf64a7b1b38cf4d6e9a6c2
BL-Vasantha <vasantha.ragimekalapalli@bridgelabz.com>
2026-06-27 11:51:10 +0530

    [VASANTHA] added UC14: Passenger Bogie Model

---
+++ App/src/model/PassengerBogie.java
@@ -0,0 +1,31 @@
+package model;
+
+
+import exception.InvalidCapacityException;
+
+public class PassengerBogie {
+
+    private String type;
+    private int capacity;
+
+    public PassengerBogie(String type, int capacity) throws InvalidCapacityException {
+
+        // ✅ Fail-Fast Validation
+        if (capacity <= 0) {
+            throw new InvalidCapacityException(
+                    "Invalid capacity for " + type + " bogie. Capacity must be greater than 0"
+            );
+        }
+
+        this.type = type;
+        this.capacity = capacity;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+}
