300a8b41d59a55e1946e25dbea6bae0cdf7524c4
BL-Vasantha <vasantha.ragimekalapalli@bridgelabz.com>
2026-06-27 14:42:39 +0530

    [VASANTHA] added UC15:Bogies Safety Controller

--- App/src/controller/SafetyController.java
+++ App/src/controller/SafetyController.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 public class SafetyController {
 
+
     private final SafetyService service;
 
     public SafetyController() {
@@ -19,7 +20,6 @@ public class SafetyController {
 
         System.out.println("\n--- Safety Compliance Result ---");
 
-        
         if (isSafe) {
             System.out.println("Train is SAFE for operation ✅");
         } else {
