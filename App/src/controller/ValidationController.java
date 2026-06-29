2b2cff2b4bbe2ff319a0f5fea0fc3202f6271d87
BL-Vasantha <vasantha.ragimekalapalli@bridgelabz.com>
2026-06-29 14:44:53 +0530

    [VASANTHA] added UC19-rain Validation Controller

--- App/src/controller/ValidationController.java
+++ App/src/controller/ValidationController.java
@@ -23,6 +23,7 @@ public class ValidationController {
             System.out.println("Train ID is INVALID: " + trainId);
         }
 
+
         if (cargoValid) {
             System.out.println("Cargo Code is VALID: " + cargoCode);
         } else {
