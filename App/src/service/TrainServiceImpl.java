227dceaf7df1ebfbfe7c2ab6d4b23401c1fc0e35
BL-Vasantha <vasantha.ragimekalapalli@bridgelabz.com>
2026-06-29 12:29:14 +0530

    [VASANTHA] added UC18-Train Service Implementation

--- App/src/service/TrainServiceImpl.java
+++ App/src/service/TrainServiceImpl.java
@@ -16,7 +16,6 @@ public class TrainServiceImpl implements TrainService {
 
 
     @Override
-
     public void displayFormation() {
         for (Map.Entry<String, Integer> entry : train.getBogieCapacityMap().entrySet()) {
             System.out.println(entry.getKey() + " -> Capacity: " + entry.getValue());
