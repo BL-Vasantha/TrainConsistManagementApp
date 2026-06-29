480b13d22eea4d2b3f05966228d738c5e45f219f
BL-Vasantha <vasantha.ragimekalapalli@bridgelabz.com>
2026-06-29 10:51:01 +0530

    [VASANTHA] added UC16-Bogie Service Interface

--- App/src/service/BogieService.java
+++ App/src/service/BogieService.java
@@ -3,12 +3,12 @@ package service;
 import model.Bogie;
 import java.util.List;
 
+
 public interface BogieService {
 
     void addBogie(Bogie bogie);
 
     List<Bogie> getAllBogies();
-
 
     int getTotalSeatingCapacity();
 }
