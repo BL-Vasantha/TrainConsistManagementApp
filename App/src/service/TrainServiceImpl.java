c31e534917683a9df4e51aaa08be6b92592dc2eb
BL-Vasantha <vasantha.ragimekalapalli@bridgelabz.com>
2026-06-29 10:34:32 +0530

    [VASANTHA] added UC16-Train Service Implementation

--- App/src/service/TrainServiceImpl.java
+++ App/src/service/TrainServiceImpl.java
@@ -9,8 +9,6 @@ public class TrainServiceImpl implements TrainService {
 
     private final Train train = new Train();
 
-
-
     @Override
     public void addBogieCapacity(String bogie, int capacity) {
         train.getBogieCapacityMap().put(bogie, capacity);
