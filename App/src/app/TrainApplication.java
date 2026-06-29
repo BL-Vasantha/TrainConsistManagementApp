602029152f31f1039afb1d1b066791fb8fed2331
BL-Vasantha <vasantha.ragimekalapalli@bridgelabz.com>
2026-06-29 14:47:40 +0530

    [VASANTHA] added UC19-Main Method

--- App/src/app/TrainApplication.java
+++ App/src/app/TrainApplication.java
@@ -1,25 +1,27 @@
 package app;
 
-
 import service.SearchService;
+
+import java.util.Arrays;
 
 public class TrainApplication {
 
     public static void main(String[] args) {
 
-        // 🔹 Unsorted Bogie IDs
-        String[] bogieIds = {"BG101", "BG205", "BG150", "BG999", "BG321"};
+        // 🔹 Sorted Bogie IDs (MANDATORY for Binary Search)
+        String[] bogieIds = {"BG101", "BG150", "BG205", "BG321", "BG999"};
 
-        String searchKey = "BG150"; // 🔍 ID to search
+        String searchKey = "BG205"; // 🔍 ID to search
 
         SearchService service = new SearchService();
 
+        System.out.println("Bogie IDs: " + Arrays.toString(bogieIds));
         System.out.println("Searching for Bogie ID: " + searchKey);
 
-        boolean found = service.searchBogieById(bogieIds, searchKey);
+        boolean found = service.binarySearchBogieId(bogieIds, searchKey);
 
         if (found) {
-            System.out.println("✅ Bogie Found in Train Consist");
+            System.out.println("✅ Bogie Found using Binary Search");
         } else {
             System.out.println("❌ Bogie Not Found");
         }
